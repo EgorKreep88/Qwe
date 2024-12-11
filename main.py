71de64229e739e832b5e4bd1d77251cb3c7eb314
Анищенко Иван Александрович <ks123anischenko@oat.local>
2024-12-11 15:57:51 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,42 +1,36 @@
 import sys
 import random
-from PyQt5 import QtWidgets, uic
-from PyQt5.QtGui import QPainter, QBrush, QColor
+from PyQt5 import uic, QtWidgets, QtGui
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget
 from PyQt5.QtCore import Qt
 
-class MainWindow(QtWidgets.QMainWindow):
+class MainWindow(QMainWindow):
     def __init__(self):
-        super(MainWindow, self).__init__()
+        super().__init__()
         uic.loadUi('ui.ui', self)
-
-        # Подключаем кнопку к обработчику
         self.pushButton.clicked.connect(self.add_circle)
+        self.setGeometry(100, 100, 800, 600)
+        self.setWindowTitle('Окружности')
 
-        # Список для хранения окружностей
-        self.circles = []
+    def paintEvent(self, event):
+        self.update()
 
     def add_circle(self):
-        # Генерация случайного диаметра и координат
-        diameter = random.randint(20, 100)
-        x = random.randint(0, self.width() - diameter)
-        y = random.randint(0, self.height() - diameter)
-
-        # Сохраняем окружность как кортеж (x, y, diameter)
-        self.circles.append((x, y, diameter))
-
-        # Перерисовываем виджет
         self.update()
 
     def paintEvent(self, event):
-        painter = QPainter(self)
-        painter.setBrush(QBrush(QColor(255, 255, 0)))  # Желтый цвет
-
-        # Рисуем все окружности
-        for (x, y, diameter) in self.circles:
+        painter = QtGui.QPainter(self)
+        for _ in range(10):  # Рисуем 10 окружностей
+            color = QtGui.QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            painter.setBrush(color)
+            diameter = random.randint(20, 100)
+            x = random.randint(0, self.width() - diameter)
+            y = random.randint(0, self.height() - diameter)
             painter.drawEllipse(x, y, diameter, diameter)
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+
